aadb31094ad2b0905b6a92a69c1e5df5acb9b393
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-17 12:33:37 -0800

    Add files via upload

---
+++ src/Sorting Algorithms/SortingAlgorithms.java
@@ -0,0 +1,40 @@
+public class SortingAlgorithms {
+
+
+    // Insertion Sort
+    private void insertionSort(int[] inputArray) {
+
+        for (int i = 0; i < inputArray.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if(inputArray[j] < inputArray[j-1]) {
+                    swap(j, j-1, inputArray);
+                }
+            }
+        }
+    }
+
+    private void selectionSort(int[] inputArray) {
+        for(int i = 0; i < inputArray.length; i++) {
+            for(int j = i + 1; j < inputArray.length; i++) {
+                
+            }
+        }
+    }
+
+
+    private void swap(int x, int y, int[] inputArray) {
+        int temp = inputArray[x];
+        inputArray[x] = inputArray[y];
+        inputArray[y] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{9,8,7,6,5,4,3,2,1};
+        SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
+
+
+        for(int i = 0; i < input.length; i++) {
+            System.out.print(input[i] + " ");
+        }
+    }
+}
