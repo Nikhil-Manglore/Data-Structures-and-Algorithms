120cbe88fca46e9aca2fb33d95665f0fa9853da2
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2021-12-20 22:38:10 -0800

    Added Bubble Sort

--- src/Sorting Algorithms/SortingAlgorithms.java
+++ src/Sorting Algorithms/SortingAlgorithms.java
@@ -1,9 +1,9 @@
+
 public class SortingAlgorithms {
 
 
-    // Insertion Sort
-    private void insertionSort(int[] inputArray) {
-
+    // Insertion Sort: O(n^2)
+    public void insertionSort(int[] inputArray) {
         for (int i = 0; i < inputArray.length; i++) {
             for (int j = i; j > 0; j--) {
                 if(inputArray[j] < inputArray[j-1]) {
@@ -13,7 +13,8 @@ public class SortingAlgorithms {
         }
     }
 
-    private void selectionSort(int[] inputArray) {
+    // Selection Sort: O(n^2)
+    public void selectionSort(int[] inputArray) {
         for(int i = 0; i < inputArray.length-1; i++) {
             int min = i;
             for(int j = i + 1; j < inputArray.length; j++) {
@@ -25,17 +26,58 @@ public class SortingAlgorithms {
         }
     }
 
+    // Bubble Sort: O(n^2)
+    public void bubbleSort(int[] inputArray) {
+        boolean repeat = true;
 
+        while(repeat) {
+            repeat = false;
+            for(int i = 0; i < inputArray.length-1; i++) {
+                if(inputArray[i] > inputArray[i+1]) {
+                    swap(i, i+1, inputArray);
+                    repeat = true;
+                }
+            }
+        }
+    }
+
+    // Heap Sort: O(nlog(n))
+    public void heapSort(int[] inputArray) {
+
+    }
+
+    // Helper Method for Heap Sort
+    private void heapify() {
+
+    }
+
+    // Merge Sort: O(nlog(n))
+    public void mergeSort(int[] inputArray) {
+        int mid = inputArray.length/2;
+
+    }
+
+    // Quick Sort: O(n^2)
+    public void quickSort() {
+
+    }
+
+    public void shellSort(int[] inputArray) {
+
+    }
+
+    // Helper Method to Swap Values in Array
     private void swap(int x, int y, int[] inputArray) {
         int temp = inputArray[x];
         inputArray[x] = inputArray[y];
         inputArray[y] = temp;
     }
 
+    // Main
     public static void main(String[] args) {
         int[] input = new int[]{9,8,7,6,5,4,3,2,1};
         SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
-        sortingAlgorithms.selectionSort(input);
+        sortingAlgorithms.bubbleSort(input);
 
         for(int i = 0; i < input.length; i++) {
             System.out.print(input[i] + " ");
