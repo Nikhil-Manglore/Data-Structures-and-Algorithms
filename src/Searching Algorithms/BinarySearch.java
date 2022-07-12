306111c382620f4fb925807c78d45259c514582a
Nikhil Manglore <46588759+Nikhil-Manglore@users.noreply.github.com>
2022-07-12 16:04:51 -0400

    Added Binary Search

---
+++ src/Searching Algorithms/BinarySearch.java
@@ -0,0 +1,27 @@
+public class BinarySearch {
+
+    // Divide and Conquer Algorithm
+    // Time Complexity: O(nlogn); Space Complexity: O(1);
+
+    private int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+
+    private int binarySearch(int[] nums, int target, int leftPointer, int rightPointer) {
+
+        int middle = leftPointer + (rightPointer - leftPointer) / 2;
+
+        if (rightPointer < leftPointer) {
+            return -1;
+        }
+
+        if (nums[middle] < target) {
+            return binarySearch(nums, target, middle + 1, rightPointer);
+        } else if (nums[middle] > target) {
+            return binarySearch(nums, target, leftPointer, middle - 1);
+        }
+
+        return middle;
+
+    }
+}
